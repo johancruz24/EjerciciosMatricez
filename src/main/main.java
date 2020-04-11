bb3946775ba0148b9068041e927041af31e42d9c
Johan Cruz <sebastiancruzsanchez@gmail.com>
2020-04-11 15:45:01 -0500

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author CruzArdila
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Crear la matriz
+        int matriz[][] = new int [5][5];
+        //Captura de valores de la Matriz de forma aleatoria.
+        for(int i = 0;i < matriz.length;i++){
+            for(int y = 0;y < matriz.length;y++){
+            
+            int numero = (int)(Math.random()*100);
+            
+            }
+        }
+            
+    }
+    
+}
